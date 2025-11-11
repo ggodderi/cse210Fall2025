b0269f50df91f6b6c94710e35525e7c42d7f8890
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-11-11 09:31:08 -0700

    Initial Circle Program

---
+++ sandbox/CircleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+class Program
+{
+    class Circle
+    {
+        private double _radius;
+        public Circle()
+        {
+            _radius = 0.0;
+        }
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+    }
+    static void Main()
+    {
+        Console.WriteLine("this is a test");
+    }
+}
